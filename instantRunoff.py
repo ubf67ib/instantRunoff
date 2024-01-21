d0bd1c2400b9abd0968ffd671f0cf07d88b762f5
NNN0 <92694053+NNN0@users.noreply.github.com>
2024-01-21 15:24:47 -0800

    Finished. Of course it's a tie.

--- instantRunoff.py
+++ instantRunoff.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 import pandas as pd
-import indexes as ind
+import utils as ut
 
 #Function to check if an array has duplicate numbers in the rows
 def checker(data):
@@ -25,26 +25,37 @@ def main():
         print("This CSV has duplicates in the rows. Fix it!")
         return 1
     
-    #Find the first picks
-    firstPicks = [0]*cols    
-    for index, row in data.iterrows():
-        row = list(row)
-        firstPicks[row.index(1)] += 1
-    
-    #Set up and do the first round's loser(s)
+    picks = [[]]
     losers = []
-    [losers.append(x) for x in ind.indexes(firstPicks, min(firstPicks))]
+    n = 0
+    #I can make it all a big loop. Oh no.
+    while n == 0 or len(set(picks[n])) > 2:
+        n += 1
 
-    #Second round time
-    secondPicks = [0]*cols
-    for index, row in data.iterrows():
-        row = list(row)
-        minVote = 1
-        while row.index(minVote) in losers:
-            minVote += 1
-        secondPicks[row.index(minVote)] += 1
-    print(secondPicks)
+        #Find the picks
+        picks.append([0]*cols)
+        for index, row in data.iterrows():
+            row = list(row)
+            minVote = 1
+            while row.index(minVote) in losers:
+                minVote += 1
+            picks[n][row.index(minVote)] += 1
+        
+        #Do the loser(s)
+        [losers.append(x) for x in ut.indexes(picks[n], ut.minNonZero(picks[n]))]
 
+        # #Second round time
+        # secondPicks = [0]*cols
+        # for index, row in data.iterrows():
+        #     row = list(row)
+        #     minVote = 1
+        #     while row.index(minVote) in losers:
+        #         minVote += 1
+        #     secondPicks[row.index(minVote)] += 1
+
+        # #Second round losers
+        # [losers.append(x) for x in ut.indexes(secondPicks, ut.minNonZero(secondPicks))]
+    [print(data.columns[x]) for x in ut.indexes(picks[n], max(picks[n]))]
 
 if __name__ == "__main__":
     main()
--- utils.py
+++ utils.py
@@ -4,3 +4,10 @@ def indexes(arr, value):
         if arr[i] == value:
             returns.append(i)
     return returns
+
+def minNonZero(arr):
+    minn = float("inf")
+    for i in arr:
+        if i < minn and i != 0:
+            minn = i
+    return int(minn)
